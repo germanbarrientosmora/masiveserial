4b9805458b8984823c7be21dcc1fa909a35bc83e
Germán Barrientos Mora <gbarrientos@soluciones4g.com>
2018-09-04 23:02:14 -0500

    [ADD] app inventory_serial_import

---
+++ inventory_serial_import/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name' : 'Import Serial Numbers on Delivery Orders',
+    'version': '1.1',
+    'author': 'PPTS [India] Pvt.Ltd.',
+    'website': 'https://www.pptssolutions.com',
+    'category': 'Inventory',
+    'license': 'LGPL-3',
+    'support': 'business@pptservices.com',
+    'license': 'LGPL-3',
+    'support': 'business@pptservices.com',
+    'depends' : ['base','stock'],
+    'description': """
+          Product Serial Numbers Import on Delivery Orders
+    """,
+    'data': [
+        
+        'views/inventory_serial_import_view.xml',
+        
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application':True
+}
